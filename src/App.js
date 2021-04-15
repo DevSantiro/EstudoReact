4209a2c15da487aeb7331c30b5c9f4680ad058ab
DevSantiro <65986475+DevSantiro@users.noreply.github.com>
2021-04-14 22:30:15 -0300

    Adicionando o select de Tags ao projeto

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
     super();
     this.state = {
       notas: [],
-      categorias: [],
+      categorias: ["Games", "Musics"],
     }
   }
 
-  criarNota(titulo, texto){
-    const novaNota   = {titulo, texto};
+  criarNota(titulo, texto, categoria){
+    const novaNota   = { titulo, texto, categoria };
     const novoArrayNotas = [...this.state.notas, novaNota]
     const novoEstado = {
       notas: novoArrayNotas
@@ -38,13 +38,19 @@ class App extends Component {
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <FormularioCadastro 
+          categorias={this.state.categorias}
+          criarNota={this.criarNota.bind(this)}
+        />
         <main className="conteudo-principal">
           <ListaDeCategorias 
             categorias={this.state.categorias} 
             adicionarCategoria={this.adicionarCategoria.bind(this)}
           />
-          <ListaDeNotas apagarNota={this.apagarNota.bind(this)} notas={this.state.notas}/>
+          <ListaDeNotas 
+            apagarNota={this.apagarNota.bind(this)} 
+            notas={this.state.notas}
+          />
         </main>
       </section>
     );
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,6 +6,7 @@ class FormularioCadastro extends Component {
     super(props)
     this.titulo = "";
     this.texto  = "";
+    this.categoria = "Sem Categoria";
   }
 
   handleMudancaTitulo(event){
@@ -18,15 +19,26 @@ class FormularioCadastro extends Component {
     this.texto  = event.target.value
   }
 
+  handleMudancaCategoria(event){
+    event.stopPropagation();
+    this.categoria  = event.target.value
+  }
+
   criarNota(event){
     event.preventDefault();
     event.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto)
+    this.props.criarNota(this.titulo, this.texto, this.categoria)
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this.criarNota.bind(this)}>
+        <select onChange={this.handleMudancaCategoria.bind(this)} className="form-cadastro_input">
+          <option>Sem Categoria</option>
+          {this.props.categorias.map( (categoria) => {
+            return <option>{categoria}</option>
+          })}
+        </select>
         <input
           type="text"
           placeholder="Título"
